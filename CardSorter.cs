d7fe54550780eff926993d53b29535c51cdc4810
NightXsenator <lapinkirill@msn.com>
2017-01-12 14:09:22 +0300

    Add files via upload

---
+++ CardSorter.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TravellerCardsNS
+{
+    public struct Card
+    {
+        private string cityA;
+        private string cityB;
+        //  string dataForTraveller;
+
+        public string CityA
+        {
+            get
+            {
+                return cityA;
+            }
+        }
+        public string CityB
+        {
+            get
+            {
+                return cityB;
+            }
+        }
+
+        public Card(string cityA, string cityB)
+        {
+            this.cityA = cityA;
+            this.cityB = cityB;
+        }
+    }
+
+    public static class CardSorter
+    {
+        public static Card[] sortCards(Card[] input)
+        {
+            #region Method Description
+            /*
+            Исходный массив помещается в список (List), расширенный
+            функционал которого позволит провести более наглядную сортировку.
+            (Оптимальной с точки зрения производительности структурой данных
+            я бы назвал двунаправленный связный список).
+            Индексами 0 и i фиксируется блок упорядоченных карточек - вначале это
+            самый первый элемент списка. Совершается обход (j) остальной части списка;
+            
+            1. Начальный город каждого элемента сравнивается с конечным городом
+            упорядоченного блока (т.е. конечным городом последнего элемента
+            упор. блока). Если одинаковы, то найденный элемент перемещается в конец
+            упорядоченного блока, блок становится больше на единицу, начинается
+            новый виток обходного цикла.
+            
+            2. Конечный город элемента сравнивается с началом упор. списка
+            (т.е. начальным городом первого эл-та в списке). Если одинаковы, то 
+            найденный элемент перемещается в начало упорядоченного блока, 
+            блок становится больше на единицу (i++), начинается новый виток обходного
+            цикла.
+            
+            ПРОИЗВОДИТЕЛЬНОСТЬ АЛГОРИТМА:
+            
+            1.) Сравнения (кол-во)
+            ХУДШ.: N^2
+            СРЕДН.: [(3/8)*N^2] 
+            
+            2.) Перестановки
+            (1/2)*(N^2)
+            
+            3.) Итого
+            ХУДШ. 3/2*(N^2)
+            АЛГОРИТМ С КВАДРАТИЧНОЙ СЛОЖНОСТЬЮ.                    
+            */
+
+            #endregion
+
+            if (input.Length == 0) throw new ArgumentNullException("Empty array");
+            if (input.Length == 1) return new Card[] { input[0] };
+
+            List<Card> sorting = new List<Card>(input);
+            for(int i=0; i<input.Length-1; i++)
+                for(int j=i+1; j<input.Length; j++)
+                {
+                    Card bkp;
+
+                    if (sorting[i].CityB == sorting[j].CityA)
+                    {
+                        bkp = sorting[j];
+                        sorting.RemoveAt(j);
+                        sorting.Insert(i + 1, bkp);
+                        break;
+                    }
+
+                    if (sorting[0].CityA == sorting[j].CityB)
+                    {
+                        bkp = sorting[j];
+                        sorting.RemoveAt(j);
+                        sorting.Insert(0, bkp);
+                        break;
+                    }
+
+                    if (j == input.Length - 1)
+                        throw new ArgumentException("Wrong cards");
+                }
+            
+            return(sorting.ToArray());
+        }
+    }
+}
---
+++ CardsSorterTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravellerCardsNS;
+
+namespace CardsTesting
+{
+    [TestClass]
+    public class CardsTestClass
+    {
+        [TestMethod]
+        public void SortAlgorhytmTest()
+        {
+            Card[] test = new Card[]
+            {
+                new Card("Vienna", "Amsterdam"),
+                new Card("Berlin", "Vienna"),
+                new Card("Tallin", "Kirov"),
+                new Card("Moscow", "Berlin"),
+                new Card("Amsterdam", "Tallin")
+            };
+
+            test = CardSorter.sortCards(test);
+            string chkval = test[0].CityA;
+            bool sortIsCorrect = true;
+            foreach (Card t in test)
+            {
+                if (t.CityA != chkval) { sortIsCorrect = false; break; }
+                chkval = t.CityB;
+            }
+
+            Assert.IsTrue(sortIsCorrect, "Sort is incorrect");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReactionOnEmptyArrayTest()
+        {
+            Card[] test = new Card[1];
+            CardSorter.sortCards(test);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ReactionOnWrongCardsCollectionTest()
+        {
+            Card[] test = new Card[]
+            {
+                new Card("Moscow", "Tallin"),
+                new Card("Peter", "Tallin")
+            };
+            CardSorter.sortCards(test);
+        }
+
+    }
+}
